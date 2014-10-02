09161fb094f2ea4db7bed90ecf8598767ff4fc6a
nebliva <nebliva@gmail.com>
2014-10-02 08:50:27 -0400

    modifications

--- processor/parallelism/parallel_processing.py
+++ processor/parallelism/parallel_processing.py
@@ -4,13 +4,20 @@ import os # on importe le module os qui dispose de variables
           # et de fonctions utiles pour dialoguer avec votre
           # systeme d'exploitation
 
+import picture
+import parallelism
+
 from PIL import Image, ImageFilter
 
 import threading
 # import time
 
 class ParallelProcessing (threading.Thread):
-    """ A COMPLETER"""
+    """ Cette classe permet de lancer et de gerer l'execution
+        de plusieurs threads. Des fonctionnalites pourront lui etre
+        rajoutees afin qu'elle permette de suspendre un thread, de 
+        l'annuler, d'accorder plus de ressources d'execution a des
+        threads donnes, etc."""
 
     def __init__(self):
 	"""Class's constructor"""
@@ -29,5 +36,10 @@ class ParallelProcessing (threading.Thread):
 	# Start all the created Threads
         for thread in threadList
 	     thread.start()
+
+      def execute_threads_cores(self, picture_iterator):
+      	    """Run a list of threads using all the CPU cores """
+            pass # completer cette fonction
+       
 	
 print "Exiting the parallel processing"
