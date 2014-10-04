0b3ba739729544872dfcb075b827bb1636a330ca
nebliva <nebliva@gmail.com>
2014-10-04 10:30:07 -0400

    amelioration du style de codage

--- picture_processor.py
+++ picture_processor.py
@@ -23,14 +23,14 @@ class BusbudBanner(object):
     @classmethod
     def load(cls, name, fp):
         """Load an image from a file pointer."""
-        #print("Loaded picture: {}".format(name) + "\n") # Added instruction
+        #print("Loaded image: {}".format(name) + "\n") # Added instruction
         return (name, Image.open(fp))
 
     @classmethod
     def save(cls, filename, image):
         """Save an image to filename"""
         image.save(filename)
-        print("The picture has been saved at : {}".format(filename) + "\n") # Added instruction 
+        print("The image has been saved at : {}".format(filename) + "\n") # Added instruction 
 
     @classmethod
     def scale_x(cls, name, image, size=1500, resample=Image.BICUBIC):
@@ -72,32 +72,32 @@ class BusbudBanner(object):
         return name + '-vmiddle', cls.crop_vertical(image, offset, y - offset)
     
     @classmethod
-    def picture_data(cls, file_name, file_object):
+    def image_data(cls, file_name, file_object):
         """Return the image to process as well as its name and its extension."""
         tuple_to_process = cls.load(file_name, file_object) # assigns an Image
         file_name = tuple_to_process[0]
-        picture_to_process = tuple_to_process[1]
+        image_to_process = tuple_to_process[1]
         drive,path_and_file = os.path.splitdrive(file_name)
         path,file = os.path.split(path_and_file)
         name, extension = file.split(".")
-        return  picture_to_process, name,  extension
+        return  image_to_process, name,  extension
     
     @classmethod
-    def scale_blur_crop(cls,  thread_index, picture_name, picture_to_process):
+    def scale_blur_crop(cls,  thread_index, imagename, image):
         """Scale, blur and crop the input image."""
-        scaled_name =  picture_name + "_scaled"
-        blurred_name =  picture_name + "_blurred"
-        print("The thread {} is scaling the picture".format(thread_index) + "\n")
-        scaled_picture = cls.scale_x(scaled_name,  picture_to_process)
+        scaled_name =  imagename + "_scaled"
+        blurred_name =  imagename + "_blurred"
+        print("The thread {} is scaling the image".format(thread_index) + "\n")
+        scaled_image = cls.scale_x(scaled_name,  image)
 
-        print("The thread {} is blurring the picture ".format(thread_index) + "\n")
-        blurred_picture = cls.blur(blurred_name, scaled_picture[1]) # scaled_picture is a tuple
+        print("The thread {} is blurring the image".format(thread_index) + "\n")
+        blurred_image = cls.blur(blurred_name, scaled_image[1]) # scaled_image is a tuple
     
-        print("The thread {} is cropping the picture".format(thread_index) + "\n")
-        top_picture = cls.crop_top(picture_name, blurred_picture[1])
-        bottom_picture = cls.crop_bottom(picture_name, blurred_picture[1])
-        middle_picture = cls.crop_vmiddle(picture_name, blurred_picture[1])
-        return   top_picture, middle_picture, bottom_picture  
+        print("The thread {} is cropping the image".format(thread_index) + "\n")
+        top_image = cls.crop_top(imagename, blurred_image[1])
+        bottom_image = cls.crop_bottom(imagename, blurred_image[1])
+        middle_image = cls.crop_vmiddle(imagename, blurred_image[1])
+        return   top_image, middle_image, bottom_image  
 
 
 class ImageThread (threading.Thread): 
@@ -112,22 +112,22 @@ class ImageThread (threading.Thread):
 
     def run(self):
         # Get the image to process as well as its name and its extension
-        picture_processor = BusbudBanner()
-        picture_to_process, filename, extension = \
-        picture_processor.picture_data(self.file.name, self.file)
+        image_processor = BusbudBanner()
+        image, filename, extension = \
+        image_processor.image_data(self.file.name, self.file)
        
         
-        # Scale, blur and crop the picture
-        top_picture, middle_picture, bottom_picture = \
-        picture_processor.scale_blur_crop(self.threadID, filename, picture_to_process)
+        # Scale, blur and crop the image
+        top_image, middle_image, bottom_image = \
+        image_processor.scale_blur_crop(self.threadID, filename, image)
 
         
-        # Save the generated pictures
+        # Save the generated images
         directory_path = "./processed_images/"
-        picture_processor.save(directory_path + top_picture[0] + "." + extension,  \
-        top_picture[1]) # top_picture is a tuple
-        picture_processor.save(directory_path +  middle_picture[0] + "." +extension,  middle_picture[1]) 
-        picture_processor.save(directory_path + bottom_picture[0] + "." +extension,  bottom_picture[1])
+        image_processor.save(directory_path + top_image[0] + "." + extension,  \
+        top_image[1]) # top_image is a tuple
+        image_processor.save(directory_path +  middle_image[0] + "." + extension,  middle_image[1]) 
+        image_processor.save(directory_path + bottom_image[0] + "." + extension,  bottom_image[1])
         
 
 class ParallelProcessing (threading.Thread):
@@ -139,15 +139,15 @@ class ParallelProcessing (threading.Thread):
 	"""Class's constructor"""
         pass
 
-    def execute_threads(self, picture_iterator):
-        """Launch concurrent threads to handle scaling, blurring
-        and crpping of images"""
-        # Create a thread per picture and start it
+    def execute_threads(self, image_iterator):
+        """Launch concurrent threads to handle the scaling, blurring
+        and cropping of images"""
+        # Create a thread per image and start it
         thread_index = 1
         threadList = []
-        for picture in picture_iterator:
-             thread = ImageThread(thread_index, picture)
-             print("The thread {} is launched to process the picture {}".format( thread_index, \
+        for image in image_iterator:
+             thread = ImageThread(thread_index, image)
+             print("The thread {} is launched to process the image {}".format(thread_index, \
              thread.file.name) + "\n")
              thread.start()
              threadList.append(thread)
@@ -161,10 +161,10 @@ class ParallelProcessing (threading.Thread):
 
 def main():
     #raise NotImplementedError
-    picture_iterator = images()  # get an iterator over the pictures to process
+    image_iterator = images()  # get an iterator over the images to process
     concurrency = ParallelProcessing() 
-    concurrency.execute_threads(picture_iterator) # launch the threads allowing the 
-                                                  # concurrent processing of the pictures
+    concurrency.execute_threads(image_iterator) # launch the threads allowing the 
+                                                # concurrent processing of the images
 
     print "Exiting Main Thread"
 
